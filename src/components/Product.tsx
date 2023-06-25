795e3b17b111355eca0d9773803988a80c55f643
tinvongoc-ct <ngoctinvo2001@gmail.com>
2023-06-25 23:06:45 +0700

    chore: add tooltip

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { ProductType } from "../types/product";
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Tooltip,
+} from "@mui/material";
 
 const Product: React.FC<ProductType> = ({
   title,
@@ -15,35 +21,34 @@ const Product: React.FC<ProductType> = ({
   images,
 }) => {
   return (
-    <Card style={{ maxWidth: 300, height: 500 }}>
-      <CardMedia component="img" height={200} image={thumbnail} alt={title} />
-      <CardContent>
-        <Typography variant="h6" gutterBottom>
-          {title}
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          {description}
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Price: {price}
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Discount: {discountPercentage}%
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Rating: {rating}
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Stock: {stock}
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Brand: {brand}
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Category: {category}
-        </Typography>
-      </CardContent>
-    </Card>
+    <Tooltip title={description}>
+      <Card style={{ maxWidth: 300, height: 500 }}>
+        <CardMedia component="img" height={200} image={thumbnail} alt={title} />
+        <CardContent>
+          <Typography variant="h6" gutterBottom>
+            {title}
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Price: {price}
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Discount: {discountPercentage}%
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Rating: {rating}
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Stock: {stock}
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Brand: {brand}
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Category: {category}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Tooltip>
   );
 };
 
